adbc200030daa883b4b3881efee5cff6f83ace98
fukusuke1026 <fukusuke.t.android@gmail.com>
2015-10-14 08:37:12 +0900

    メソッドの追加

--- src/main/scala/com/scalastudy/chapter6/Rational.scala
+++ src/main/scala/com/scalastudy/chapter6/Rational.scala
@@ -26,14 +26,53 @@ class Rational(n: Int, d: Int) {
    * @param that 加算対象の分数クラス
    * @return Rational 加算後の分数クラス
    */
-  def add(that:Rational):Rational = new Rational(numer * that.denom + that.numer * denom , denom * that.denom)
+  def + (that:Rational):Rational =
+    new Rational(numer * that.denom + that.numer * denom , denom * that.denom)
+
+  /**
+   * 加算
+   * @param i 加算対象の整数クラス
+   * @return Rational 加算後の分数クラス
+   */
+  def + (i:Int): Rational = {
+    //this.+(new Rational(i))
+    new Rational(numer + i * denom, denom )
+  }
+
+  /**
+   * 減算
+   * @param that　減算対象の分数クラス
+   * @return　Rational 減算後の分数クラス
+   */
+  def - (that:Rational):Rational =
+    new Rational(numer * that.denom - that.numer * that.denom, denom * that.denom)
+
+
+  /**
+   * 乗算
+   * @param that　乗算対象の分数クラス
+   * @return　Rational 乗算後の分数クラス
+   */
+  def * (that:Rational): Rational =
+    new Rational(numer * that.numer, denom * that.denom)
+
+
+  /**
+   * 除算
+   * @param that　除算対象の分数クラス
+   * @return　Rational 除算後の分数クラス
+   */
+  def / (that:Rational):Rational =
+    new Rational(numer * that.denom, denom * that.numer)
+
 
   /**
    * 自身のほうが小さいかの判定
    * @param that 比較対象の分数クラス
    * @return Boolean 自身が小さい場合はtrue
    */
-  def lessThan(that: Rational):Boolean = this.numer * that.denom < that.numer * this.denom
+  def lessThan(that: Rational):Boolean =
+    this.numer * that.denom < that.numer * this.denom
 
   /**
    * 最大公約数の計算
